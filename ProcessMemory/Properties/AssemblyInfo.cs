bee328730397598f333b1758d087aa387eeabf55
mat1jaczyyy <mat1jaczyyy@gmail.com>
2026-07-22 00:10:24 +0200

    Modernize ProcessMemory for net472

--- ProcessMemory/Properties/AssemblyInfo.cs
+++ ProcessMemory/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ProcessMemory")]
-[assembly: AssemblyDescription("A Process Memory reader and writer for .NET")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A Windows x64 process memory reader and writer for .NET Framework")]
 [assembly: AssemblyCompany("mat1jaczyyy")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyProduct("ProcessMemory")]
+[assembly: AssemblyCopyright("Copyright (c) 2019-2026 mat1jaczyyy")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("35b5c872-d829-428f-9dfa-8ef2b4de194c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ProcessMemory
+
+ProcessMemory is a small Windows x64 library for reading, writing, allocating, and traversing memory in another process.
+
+Version 1.3 targets .NET Framework 4.7.2. The public API remains compatible with ProcessMemory 1.2.1.
+
+## Install
+
+```powershell
+dotnet add package ProcessMemory --version 1.3.0
+```
+
+The consuming application must run as x64 and may require elevated permissions to open its target process.
+
+## Example
+
+```csharp
+var memory = new ProcessMemory("MyGame");
+
+if (memory.CheckProcess()) {
+    var baseAddress = new IntPtr(memory.getBaseAddress);
+    var value = memory.ReadInt32(baseAddress + 0x1234);
+    memory.WriteInt32(baseAddress + 0x1234, value + 1);
+}
+```
